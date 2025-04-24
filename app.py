fcb7be470dc0196982a6ce038be520fc58771c90
LOKESH <91692281+LOKESH780@users.noreply.github.com>
2025-04-24 15:49:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def add_bg_from_local(image_file):
     st.markdown(f"""
         <style>
         .stApp {{
-            background-image: url("data:image/peg;base64,{encoded}");
+            background-image: url("data:image/png;base64,{encoded}");
             background-size: cover;
             background-position: center;
         }}
@@ -30,7 +30,7 @@ def add_bg_from_local(image_file):
         </style>
     """, unsafe_allow_html=True)
 
-add_bg_from_local("background.peg")
+add_bg_from_local("wallpaper.png")
 
 # === Login Logic ===
 if "logged_in" not in st.session_state:

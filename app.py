bf0eff14d1e0fcadd81783da61ac2ee092fb73f2
LOKESH <91692281+LOKESH780@users.noreply.github.com>
2025-04-30 00:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from login import login
 from io import BytesIO
 import base64
